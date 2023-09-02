dbab328ae6fb919df1d43ad716a87eb6d5c9739d
teste-agua-edu <gui.2001@hotmail.com>
2023-09-02 05:13:44 -0300

    feat: dropdown account created

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,40 +1,42 @@
 'use client'
 
 import { styled } from "styled-components";
-import Logo from "./Logo";
-import CartInfo from "./cart-info";
-import AccountInfo from "./account-info";
+
+import { Logo } from "./icons";
+
+import { CartInfo } from "../components/Cart";
+
+import { AccountInfo } from "../components/Account";
 
 export default function Header() {
 
- const navItem = [
-  {
-   id: 0,
-   name: 'Início',
-   url: '/',
-  },
-  {
-   id: 1,
-   name: 'Comprar',
-   url: '/',
-  },
-  {
-   id: 2,
-   name: 'Contato',
-   url: '/',
-  },
-  {
-   id: 3,
-   name: 'Ajuda',
-   url: '/',
-  },
- ];
+  const navItem = [
+    {
+      id: 0,
+      name: 'Início',
+      url: '/',
+    },
+    {
+      id: 1,
+      name: 'Comprar',
+      url: '/',
+    },
+    {
+      id: 2,
+      name: 'Contato',
+      url: '/',
+    },
+    {
+      id: 3,
+      name: 'Ajuda',
+      url: '/',
+    },
+  ];
 
- const Navbar = styled.header`
-  border-bottom: 1px solid var(--color-gray-50);
+  const Navbar = styled.header`
+  border-bottom: 1px solid var(--color-gray-100);
   
-  
-  .container_header_box {
+  nav {
     height: 80px;
     display: flex; 
     margin: 0 auto;
@@ -45,49 +47,50 @@ export default function Header() {
 
   .account-details {
    display: flex;
+   cursor: pointer;
    gap: 56px;
   }
+`;
 
- `;
-
- const NavList = styled.ul`
+  const NavList = styled.ul`
   gap: 48px;
   display: flex;
 
   li {
-   a {
-    font-size: 16px;
-    font-weight: 500;
-    font-style: normal;
-    line-height: normal;
-    text-decoration: none;
-    color: inherit;
-   }
+    a {
+      cursor: pointer;
+      font-size: 16px;
+      font-weight: 500;
+      font-style: normal;
+      line-height: normal;
+      text-decoration: none;
+      color: var(--color-gray-300);
+    }
   }
  `;
 
- return (
-  <Navbar>
-   <div className="container_header_box">
-    <Logo />
+  return (
+    <Navbar>
+      <nav>
+        <Logo />
 
-    <NavList>
-     {navItem.map((item, indx) => (
-      <li key={item.id}>
-       <a>
-        {item.name}
-       </a>
-      </li>
-     ))}
-    </NavList>
+        <NavList>
+          {navItem.map((item) => (
+            <li key={item.id}>
+              <a>
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </NavList>
 
-    <div className="account-details">
-     <AccountInfo />
+        <div className="account-details">
+          <AccountInfo />
 
-     <CartInfo />
-    </div>
-   </div>
+          <CartInfo />
+        </div>
+      </nav>
 
-  </Navbar>
- )
+    </Navbar>
+  )
 }
---
+++ src/app/components/icons/index.tsx
@@ -0,0 +1,22 @@
+import IconCart from "./icon-cart";
+import IconCheck from "./icon-check";
+import Logo from "./icon-logo";
+import IconSeparetor from "./icon-date-separator";
+import IconArrow from "./icon-arrow";
+import IconProfile from "./icon-profile";
+import IconShoppings from "./icon-shoppings";
+import IconSettings from "./icon-settings";
+import IconLogout from "./icon-logout";
+
+
+export {
+ IconLogout,
+ IconSettings,
+ IconSeparetor,
+ IconShoppings,
+ IconProfile,
+ IconArrow,
+ IconCheck,
+ IconCart,
+ Logo,
+}
