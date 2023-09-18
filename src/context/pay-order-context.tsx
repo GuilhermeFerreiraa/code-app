6503fcb4dd83e1613a2876171e5c82fa7efda406
teste-agua-edu <gui.2001@hotmail.com>
2023-09-18 00:40:36 -0300

    feat: remove useLocalStorage to add ContextAPI

--- src/context/pay-order-context.tsx
+++ src/context/pay-order-context.tsx
@@ -5,6 +5,10 @@ import React, { createContext, useState } from "react";
 export const StepOrder = createContext({
  stepOrder: 0,
  setStepOrder: (value: number | any) => { },
+ isLoading: false,
+ setIsLoading: (value: boolean | any) => { },
+ orderSize: 0,
+ setOrderSize: (value: number | any) => { },
 })
 
 interface ProviderProps {
@@ -13,9 +17,20 @@ interface ProviderProps {
 
 export function PayOrderProvider({ children }: ProviderProps) {
  const [stepOrder, setStepOrder] = useState(0);
+ const [isLoading, setIsLoading] = useState(false);
+ const [orderSize, setOrderSize] = useState(1);
+
+ const values = {
+  stepOrder,
+  setStepOrder,
+  orderSize,
+  setOrderSize,
+  isLoading,
+  setIsLoading
+ }
 
  return (
-  <StepOrder.Provider value={{ stepOrder, setStepOrder }}>
+  <StepOrder.Provider value={values}>
    {children}
   </StepOrder.Provider>
  )
--- src/utils/formatCurrency.tsx
+++
@@ -1,8 +0,0 @@
-export function formatCurrency(value: number) {
- const formatter = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL'
- });
-
- return formatter.format(value);
-}
