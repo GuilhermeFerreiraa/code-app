0aea90b3805b95444185206a35a2a5e1a2f1f068
teste-agua-edu <gui.2001@hotmail.com>
2023-09-18 00:47:20 -0300

    refactor: Input default has been changed

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,33 +1,33 @@
 import styled from 'styled-components';
 
 export const Input = styled.input` 
-      border: 1px solid var(--color-gray-200);
-      background: var(--color-gray-50);
-      color: var(--color-gray-500);
-      transition: all .2s ease-in;
-      line-height: normal;
-      font-style: normal;
-      border-radius: 5px;
-      padding: 16px 19px;
-      font-weight: 500;
-      font-size: 16px;
-      max-height: 59px;
-      outline: none;
+  border: 1px solid var(--color-gray-200);
+  background: var(--color-gray-50);
+  color: var(--color-gray-500);
+  transition: all .2s ease-in;
+  line-height: normal;
+  font-style: normal;
+  border-radius: 5px;
+  padding: 14px 19px;
+  font-weight: 500;
+  font-size: 14px;
+  max-height: 59px;
+  outline: none;
 
-      &:hover{
-        border: 1px solid var(--color-gray-100);
-      }
+  &:hover{
+    border: 1px solid var(--color-gray-100);
+  }
 
-      &:active {
-        border: 1px solid var(--color-gray-100);
-      }
-      
-      &:focus-visible {
-        box-shadow: 0px 0px 10px -3px #00000025;
-        border: 1px solid var(--color-gray-100);
-      }
-      
-      &::placeholder{
-        color: var(--color-gray-400);
-      }
- `;
+  &:active {
+    border: 1px solid var(--color-gray-100);
+  }
+  
+  &:focus-visible {
+    box-shadow: 0px 0px 10px -3px #00000025;
+    border: 1px solid var(--color-gray-100);
+  }
+  
+  &::placeholder{
+    color: var(--color-gray-400);
+  }
+`;
