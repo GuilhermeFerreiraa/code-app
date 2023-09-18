403459552a72610309c0ea53669041957cc9f294
teste-agua-edu <gui.2001@hotmail.com>
2023-09-18 00:44:02 -0300

    refactor: FormGroup

--- src/app/components/FormGroup.tsx
+++ src/app/components/FormGroup.tsx
@@ -1,11 +1,12 @@
 'use client'
 
+import breakpoints from '@/utils/breakpoints';
 import React, { ReactNode } from 'react';
 import styled from 'styled-components'
 
 interface FormProps {
   children: ReactNode,
-  text: string,
+  text?: string,
   error?: string | boolean | undefined,
 }
 
@@ -15,17 +16,43 @@ const StyledFormGroup = styled.div`
   
   .title {
       padding-bottom: 12px;
-      font-size: 16px;
+      font-size: 14px;
       font-weight: 500;
       font-style: normal;
       line-height: normal;
       padding-bottom: 11px;
       color: var(--color-gray-400);
-    }
+  }
 
   .error {
-   margin-top: 12px; 
-   color: var(--color-error);
+    display: flex;
+    flex-direction: column;
+
+    small {
+    margin-top: 12px; 
+    color: var(--color-error);
+  }
+
+  input {
+    color: var(--color-error);
+    border: 1px solid var(--color-error);
+    box-shadow: 0px 0px 5px -3px var(--color-error);
+    
+    &::placeholder {
+      color: var(--color-error);
+    }
+  }
+  }
+
+  @media screen and (${breakpoints.device.xs}) and (max-width: ${breakpoints.size.sm}) {
+    .title {
+      font-size: 14px;
+    } 
+
+    input {
+      padding: 14px;
+      font-size: 14px;
+    }
   }
 `;
 
@@ -33,8 +60,16 @@ const FormGroup = (props: FormProps) => {
   return (
     <StyledFormGroup>
       {props.text && <small className="title">{props.text}</small>}
-      {props.children}
-      {props.error && <small className="error">{props.error}</small>}
+
+
+      {!props.error ? props.children : (
+        <div className="error">
+          {props.children}
+          <small>
+            {props.error}
+          </small>
+        </div>
+      )}
     </StyledFormGroup>
   )
 }
